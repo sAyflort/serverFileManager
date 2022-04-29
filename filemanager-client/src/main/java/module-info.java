deabfa486bb65aab2454cf53ccad29c32b6e82e4
sAyflort <neefe.saratan.life.life@gmail.com>
2022-04-29 11:07:03 +0300

    new error

--- filemanager-client/src/main/java/module-info.java
+++ filemanager-client/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.filemanagerclient {
     requires javafx.controls;
     requires javafx.fxml;
     requires io.netty.all;
+    requires io.netty.transport;
 
     opens ru.geekbrains.serverFileManager to javafx.fxml;
     exports ru.geekbrains.serverFileManager;
